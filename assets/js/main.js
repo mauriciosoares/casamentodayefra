05978301cf3a8c42046934669eb8dc03ec0f2adc
Mauricio Soares <m.soares2603@gmail.com>
2016-02-09 10:32:07 -0200

    updates

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,8 @@
 setInterval(countdownWedding, 1000);
 
 function countdownWedding() {
-  var c = moment("2016-10-25").countdown();
+  var c = moment("2016-11-13").countdown();
+  document.getElementById('months').innerText = c.months;
   document.getElementById('days').innerText = c.days;
   document.getElementById('hours').innerText = c.hours;
   document.getElementById('minutes').innerText = c.minutes;
